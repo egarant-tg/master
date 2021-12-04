09f5a377848a3d5e6fc3c8aa672e1cad557448a4
Picklestan <antongarantirin@yahoo.com>
2021-12-04 12:26:58 +0300

    ,

--- parser_egarant_limiti.py
+++ parser_egarant_limiti.py
@@ -1,4 +1,4 @@
-async def parser_egarant_limiti(text):
+async def parse_egarant_limiti(text):
     for line in text:
         if "Количество СК в РСА: " in line: #настройки для "rsamonitor"
             await client.send_message("svatut", "Все работает")
