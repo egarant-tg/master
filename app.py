43064f23328d1dc8a2e6c7628acddd35342da417
Picklestan <antongarantirin@yahoo.com>
2022-02-10 19:27:55 +0300

    new string token

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from parser_egarant_limiti import parse_egarant_limiti
 api_id = "5086045"
 api_hash = "e0bcc2b6aba38988f6675767aee63591"
 #string_session  =  "1ApWapzMBu62vBenZBzr-NOhGlR3BlVst4V_8nKC0Vag1n5hI7TUY0stSet4-wCV218Q64Y6cbYpOzRVkC0_SmiXp5JZijwclv21ls09lq-j6_WL5okhC9yv_3B94pERxUCgtLjy5Zo6Dgd-qnlpNbASsf8qLoKQq6HnnFmjH_PvkwcSQVoHJS1GKRSrkLznMdFp1DXoTMTdkNXLTiGqxVdRZ8AkrROeuzrKVIwiG5H_inO8flZ5pElgJx0RkBg8LDdYXkUGYDNQHUllqjio9w9960TrQItFfaJCW2CsqYyt6batETFZOqbtxDSxkOEkEJbiq7gcR-WRzegKCZhxREeltTGaPMUs="
-string_session = "1ApWapzMBu2f2kSOeSv5_ylPQvqHW9ti3UvFwJm9seNAUyYRhmGoO7bsorJdqULdJb_2xaiv5HTHXBjVqnMZri_pDnc9EGh-FQ0Fpr3OiKtWcMNSrTdTU3elYhLk6niCtvt5-wiYfMTAfoVZcKNEGDskpkNauzfUCLyA5H5JfNGlxJlWbw5s1-tLCLOORCzVHsqf37grRo14AM00e9UioBDsQ6GitamrB2iSz3e0_UbhugtsuvCghE7y4VXb17L34VqeSM481AQO0vw_c0RZ7olPqUC_ywluueIxGh31EgtpouiDtKYcYh4rplJPsFh-cX8reATA5meBIa1BsBqxmDgvoeL11vhM="
+string_session = "1ApWapzMBu0bZla1lYqDwSJnRI6nasEwABF4LOxOxSFq6RjzSEvHizF4mTk94Mw9r3XXOGjKGCafF6jt6V7Ps7TP3ri1yt3xThYGatUGqLntxrRoYx7WK1Ry786BVwYw2v9is0pMMWUGR91SsRXrCL2-Bcmrmi-HKEq22bA-yU6CdXe--TzOCsLZJH3Q4McOXYsHikTZjIgikVSdtKKa9PSrFkHbGH_a9S-ohiwGKCN6ZEsGnRsww3g55Q5o4rr6hfPrg4Pp_Pcf1E-_TV5BJxovEbwoaqQeSC0NMtZEOD50aRD1eeV5PZdAPhYMMctJERXE3vbEYH0hMHHWd2W1Xhh5NQraQS3U="
 #аккаунт получателя алярма
 alarm_receiver = "IC_quantity"
 #название канала
@@ -32,7 +32,8 @@ max_quotes = 1
 max_polis = 2
 time = datetime.datetime.now()
 
-client = TelegramClient(StringSession(string_session), api_id, api_hash)
+
+cd desktop/tgapp/mastetclient = TelegramClient(StringSession(string_session), api_id, api_hash)
 #client = TelegramClient("anon", api_id, api_hash)
 client.start()
 print("Client Created")
